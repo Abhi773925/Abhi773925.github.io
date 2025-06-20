dbffa54af850bcd30e7b5c97d1745be4186a1227
Abhi773925 <rockabhisheksingh778189@gmail.com>
2025-06-20 10:05:28 +0530

    js

---
+++ javascript/file1.js
@@ -0,0 +1,8 @@
+let age=20;
+let name="John";
+let promoted=true;
+let score;
+let city=null;
+console.log(name);
+console.log(typeof(score))
+console.log(typeof promoted);   
---
+++ javascript/file2.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 3;
+let str = "Hello";
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);
+
+console.log("Addition with string:", a + str);
+console.log("Subtraction with string:", a - str);
+console.log("Multiplication with string:", a * str);
+console.log("Division with string:", a / str);
+console.log("Modulus with string:", a % str);
+
+console.log("String + String:", str + str);
+console.log("String * String:", str * str);
